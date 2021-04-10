4536207e467d4453b37d67016ef002c61570e593
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2021-04-10 16:51:55 -0500

    first mobile app react-native

---
+++ components/Caritem/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, ImageBackground } from "react-native";
+import styles from "./styles";
+import StyledButton from "../StyledButton";
+
+const CarItem = (props) => {
+  const { name, tagLine, image, tagLineCTA } = props.car;
+
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground source={image} style={styles.image} />
+      <View style={styles.titles}>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>
+          {tagLine} <Text style={styles.subtitleCTA}>{tagLineCTA}</Text>
+        </Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <StyledButton
+          type="primary"
+          content={"Custom Order"}
+          onPress={() => {}}
+        />
+        <StyledButton
+          type="secondary"
+          content={"Existing Inventory"}
+          onPress={() => {}}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default CarItem;
